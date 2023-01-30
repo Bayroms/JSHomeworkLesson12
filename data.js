2e7b55ecfddb7d4084b48f17142241c4fef1705b
roman <roman3668@gmail.com>
2023-01-30 16:50:26 +0300

    HW Lesson 12

---
+++ data.js
@@ -0,0 +1,55 @@
+const dataInfo = `[{
+    "id": "MANGO PEOPLE T-SHIRT",
+    "image": "image/content_img1.jpg",
+    "price": " $52.00",
+    "color": "Color: Red",
+    "size": "Size: Xl",
+    "count": "1",
+    "discription": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+},
+{
+    "id": "ELLERY X M'O CAPSULE",
+    "image": "image/content_img2.jpg",
+    "price": "$52.00",
+    "color": "Color: black",
+    "size": "Size: Xl",
+    "count": "1",
+    "discription": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+},
+{
+    "id": "ELLERY X M'O CAPSULE",
+    "image": "image/content_img3.jpg",
+    "price": "$52.00",
+    "color": "Color: black",
+    "size": "Size: Xl",
+    "count": "1",
+    "discription": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+},
+{
+    "id": "ELLERY X M'O CAPSULE",
+    "image": "image/content_img4.jpg",
+    "price": "$52.00",
+    "color": "Color: black",
+    "size": "Size: Xl",
+    "count": "1",
+    "discription": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+},
+{
+    "id": "ELLERY X M'O CAPSULE",
+    "image": "image/content_img5.jpg",
+    "price": "$52.00",
+    "color": "Color: black",
+    "size": "Size: Xl",
+    "count": "1",
+    "discription": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+},
+{
+    "id": "ELLERY X M'O CAPSULE",
+    "image": "image/content_img6.jpg",
+    "price": "$52.00",
+    "color": "Color: black",
+    "size": "Size: Xl",
+    "count": "1",
+    "discription": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+}
+]`
